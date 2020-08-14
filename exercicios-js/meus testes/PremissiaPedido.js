91f47c47af5ad4006f41326c1589cea06c5d5213
brendo <brendodj@hotmail.com>
2020-08-14 10:53:05 -0300

    HTML

--- exercicios-js/meus testes/PremissiaPedido.js
+++ exercicios-js/meus testes/PremissiaPedido.js
@@ -1,11 +1,11 @@
 function valorPedido(picanha,medalhao,contra,linguica){
 const entrega = 5
 const Picanha = 3.3 * picanha
-const Medalhao = 2.9 * medalhao
+const Medalhao = 3 * medalhao
 const Contra = 3.3 * contra
 const Linguica = 3 * linguica
 const total = Picanha + Medalhao + Contra + Linguica + entrega
     console.log(total)
 }
 
-valorPedido(0, 40, 60, 0)
+valorPedido(0, 40, 80, 0)
